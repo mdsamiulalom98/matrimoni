d554e80c5b04800fe0344f6cce4af43edbe7a6ed
mdsamiulalom <samiulalomsdp98@gmail.com>
2025-04-21 00:18:33 +0600

    21-april-25-00-18

--- resources/views/frontEnd/layouts/pages/members.blade.php
+++ resources/views/frontEnd/layouts/pages/members.blade.php
@@ -190,7 +190,7 @@
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active mt-3" id="my-match" role="tabpanel" aria-labelledby="my-match-tab">
 
-                    <div class="find_member">
+                    <div class="find_member d-none">
                         <div class="filtercontainer">
                             <form action="#" method="GET">
                                 <div class="searchbox">
@@ -449,13 +449,15 @@
             <!-- Popup 1: Basic Information -->
             <div id="popup1" class="popup-container">
                 <div class="popup">
-                    <fieldset>
+                    <fieldset id="basicInfo">
                         <div class="pop_up_head_icon">
                             <i class="fa-solid fa-user"></i>
                         </div>
-                        <h2>Basic Information</h2>
+                        <div class="title-wrapper">
+                            <h2>Basic Information</h2>
+                            <p>30%</p>
+                        </div>
                         <div class="row">
-
                             <div class="col-sm-12">
                                 <h6 class="my-1" style="text-align: start;font-size:14px;font-weight:bold">Profile
                                     Created By *</h6>
@@ -467,7 +469,7 @@
                                     <div class="toggle-btn" data-value="5">Friend</div>
                                     <div class="toggle-btn" data-value="6">Others</div>
                                 </div>
-                                <input type="hidden" name="profile_created_by" id="selectedProfile">
+                                <input type="hidden" name="profile_created_by" id="selectedProfile" required>
                             </div>
                             <div class="col-sm-12">
 
@@ -477,7 +479,7 @@
                                     <div class="toggle-btn" data-value="1">Bride</div>
                                     <div class="toggle-btn" data-value="2">Groom</div>
                                 </div>
-                                <input type="hidden" name="looking_for" id="selectedLookingFor">
+                                <input type="hidden" name="looking_for" id="selectedLookingFor" required>
                             </div>
                             <div class="col-sm-12">
                                 {{-- <div class="form-group">
@@ -497,13 +499,13 @@
                                                 {{ $religion->title }}</div>
                                         @endforeach
                                     </div>
-                                    <input type="hidden" name="religion_id" id="selectedReligion">
+                                    <input type="hidden" name="religion_id" id="selectedReligion" required>
                                 </div>
                             </div>
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Profession *</label>
-                                    <select name="profession_id">
+                                    <select name="profession_id" required>
                                         <option value="">Select Profession</option>
                                         @foreach ($professions as $profession)
                                             <option value="{{ $profession->id }}">{{ $profession->title }}</option>
@@ -529,7 +531,7 @@
                                 <div class="form-group">
                                     <label>Date of Birth *</label>
                                     <div class="dob-group">
-                                        <select name="year">
+                                        <select name="year" required>
                                             <option value="">Year</option>
                                             @php
                                                 $currentYear = date('Y');
@@ -539,13 +541,13 @@
                                                 <option value="{{ $i }}">{{ $i }}</option>
                                             @endfor
                                         </select>
-                                        <select name="month">
+                                        <select name="month" required>
                                             <option value="">Month</option>
                                             @foreach ($months as $month)
                                                 <option value="{{ $month->month }}">{{ $month->title }}</option>
                                             @endforeach
                                         </select>
-                                        <select name="day">
+                                        <select name="day" required>
                                             <option value="">Day</option>
                                             @for ($i = 1; $i <= 31; $i++)
                                                 <option
@@ -562,7 +564,7 @@
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Education *</label>
-                                    <select name="education_id">
+                                    <select name="education_id" required>
                                         <option value="">Select Education</option>
                                         @foreach ($educations as $education)
                                             <option value="{{ $education->id }}">{{ $education->title }}</option>
@@ -572,8 +574,8 @@
                             </div>
                         </div>
                     </fieldset>
-                    <button type="button" onclick="nextPopup(1, 2)">Next</button>
-                    <span class="close-btn" onclick="closePopup(1)"><i class="fa-solid fa-arrow-left"></i></span>
+                    <button type="button" id="basicNext" disabled onclick="nextPopup(1, 2)">Next</button>
+                    {{-- <span class="close-btn" onclick="closePopup(1)"><i class="fa-solid fa-arrow-left"></i></span> --}}
                     {{-- <span class="close-btn" onclick="closePopup(1)">&times;</span> --}}
                 </div>
             </div>
@@ -583,16 +585,19 @@
                 <div class="popup">
                     <span class="close-btn" onclick="closePopup(2)"><i class="fa-solid fa-arrow-left"></i></span>
                     <!-- Present Location -->
-                    <fieldset>
+                    <fieldset id="presentLocation">
                         <div class="row">
                             <div class="pop_up_head_icon">
                                 <i class="fa-solid fa-location-dot"></i>
                             </div>
-                            <h2>Present Location</h2>
+                            <div class="title-wrapper">
+                                <h2>Present Location</h2>
+                                <p>60%</p>
+                            </div>
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Country *</label>
-                                    <select name="country_id">
+                                    <select name="country_id" required>
                                         @foreach ($countries as $country)
                                             <option value="{{ $country->id }}">{{ $country->title }}</option>
                                         @endforeach
@@ -602,7 +607,7 @@
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Present Division *</label>
-                                    <select name="present_division">
+                                    <select name="present_division" required>
                                         <option value="">Select Division</option>
                                         @foreach ($divisions as $division)
                                             <option value="{{ $division->id }}">{{ $division->title }}</option>
@@ -614,7 +619,7 @@
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Present District *</label>
-                                    <select name="present_district">
+                                    <select name="present_district" required>
                                         <option value="">Select District</option>
                                         @foreach ($districts as $district)
                                             <option value="{{ $district->id }}">{{ $district->title }}</option>
@@ -625,7 +630,7 @@
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Present Upazila *</label>
-                                    <select name="present_upazila">
+                                    <select name="present_upazila" required>
                                         <option value="">Select Upazila</option>
                                         @foreach ($upazilas as $upazila)
                                             <option value="{{ $upazila->id }}">{{ $upazila->title }}</option>
@@ -642,7 +647,7 @@
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Residency Status *</label>
-                                    <select name="residency_id">
+                                    <select name="residency_id" required>
                                         <option value="">Select Residency</option>
                                         @foreach ($countries as $country)
                                             <option value="{{ $country->id }}">{{ $country->title }}</option>
@@ -652,7 +657,7 @@
                             </div>
                         </div>
                     </fieldset>
-                    <button type="button" onclick="nextPopup(2, 3)">Next</button>
+                    <button id="presentNext" disabled type="button" onclick="nextPopup(2, 3)">Next</button>
                     <button type="button" onclick="prevPopup(2, 1)">Back</button>
                 </div>
             </div>
@@ -661,12 +666,15 @@
             <div id="popup3" class="popup-container">
                 <div class="popup">
                     <span class="close-btn" onclick="closePopup(3)"><i class="fa-solid fa-arrow-left"></i></span>
-                    <fieldset>
+                    <fieldset id="accountInfo">
                         <div class="row">
                             <div class="pop_up_head_icon">
                                 <i class="fa-solid fa-circle-info"></i>
                             </div>
-                            <h2>Account Information</h2>
+                            <div class="title-wrapper">
+                                <h2>Account Information</h2>
+                                <p>100%</p>
+                            </div>
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label>Email Address *</label>
@@ -738,7 +746,7 @@
                         <!-- ./ col -->
                     </fieldset>
                     <button type="button" onclick="prevPopup(3, 2)">Back</button>
-                    <button type="submit" onclick="submitForm()">Register</button>
+                    <button disabled id="registerButton" type="submit" onclick="submitForm()">Register</button>
                 </div>
             </div>
         </form>
@@ -819,5 +827,39 @@
                 }
             });
         });
+    </script>
+    <script>
+        function enableButtonIfValid(containerSelector, buttonSelector) {
+            function check() {
+
+                let allFilled = true;
+
+                $(`${containerSelector} [required]`).each(function() {
+                    if ($(this).is('select')) {
+                        if ($(this).val() === '') {
+                            allFilled = false;
+                            return false;
+                        }
+                    } else {
+                        if ($.trim($(this).val()) === '') {
+                            allFilled = false;
+                            return false; // exit loop early
+                        }
+                    }
+                });
+
+                $(buttonSelector).prop('disabled', !allFilled);
+            }
+
+            // Run check on input changes
+            $(`${containerSelector} [required]`).on('input change', check);
+
+            // Initial check in case some inputs are pre-filled
+            $(document).ready(check);
+        }
+
+        enableButtonIfValid('#basicInfo', '#basicNext');
+        enableButtonIfValid('#presentLocation', '#presentNext');
+        enableButtonIfValid('#accountInfo', '#registerButton');
     </script>
 @endpush
